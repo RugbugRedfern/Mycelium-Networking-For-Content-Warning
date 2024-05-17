b3a1d4ec9f1aa00d8afcd212b30aaa7390143a95
unknown <noccqlive@gmail.com>
2024-05-17 13:24:39 -0300

    fix: Use Invariant Culture for object to string conversion

--- MyceliumNetworkingForCW/MyceliumNetwork.cs
+++ MyceliumNetworkingForCW/MyceliumNetwork.cs
@@ -1,6 +1,7 @@
 ﻿using Steamworks;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -372,7 +373,9 @@ namespace MyceliumNetworking
 				RugLogger.LogWarning($"Accessing lobby data for unregistered key '{key}'. This might not exist.");
 			}
 
-			if(!SteamMatchmaking.SetLobbyData(Lobby, key, value.ToString()))
+            
+
+            if (!SteamMatchmaking.SetLobbyData(Lobby, key, (string)Convert.ChangeType(value, typeof(string), CultureInfo.InvariantCulture)))
 			{
 				RugLogger.LogError("Error setting lobby data.");
 			}
@@ -424,7 +427,7 @@ namespace MyceliumNetworking
 
 			try
 			{
-				return (T)Convert.ChangeType(value, typeof(T));
+				return (T)Convert.ChangeType(value, typeof(T), CultureInfo.InvariantCulture);
 			}
 			catch(Exception ex)
 			{
@@ -452,7 +455,7 @@ namespace MyceliumNetworking
 				RugLogger.LogWarning($"Accessing player data for unregistered key '{key}'. This might not exist.");
 			}
 
-			SteamMatchmaking.SetLobbyMemberData(Lobby, key.ToString(), value.ToString());
+			SteamMatchmaking.SetLobbyMemberData(Lobby, key.ToString(), (string)Convert.ChangeType(value, typeof(string), CultureInfo.InvariantCulture));
 		}
 
 		/// <summary>
@@ -503,7 +506,7 @@ namespace MyceliumNetworking
 			{
 				try
 				{
-					return (T)Convert.ChangeType(value, typeof(T));
+					return (T)Convert.ChangeType(value, typeof(T), CultureInfo.InvariantCulture);
 				}
 				catch(Exception ex)
 				{
